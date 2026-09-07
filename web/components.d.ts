22f5800c3aa6dfe7e0042e2fa184fe60657c848f
slowleelab <305525396+slowleelab@users.noreply.github.com>
2026-09-07 10:52:24 +0800

    chore(web): 构建产物 components.d.ts 更新 + UI 验证截图归档

--- web/components.d.ts
+++ web/components.d.ts
@@ -49,6 +49,8 @@ declare module 'vue' {
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElStep: typeof import('element-plus/es')['ElStep']
+    ElSteps: typeof import('element-plus/es')['ElSteps']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
